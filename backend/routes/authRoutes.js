11f84524c5e6e5a214ab64af71460ba2063947ae
Risidu03 <geeganagerisidu@gmail.com>
2026-03-10 03:04:50 +0530

    Add login API with JWT authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const router = express.Router();
 
+
+// REGISTER
 router.post("/register", async (req, res) => {
 
   try {
@@ -27,6 +30,43 @@ router.post("/register", async (req, res) => {
     await user.save();
 
     res.status(201).json({ message: "User registered successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+});
+
+
+// LOGIN
+router.post("/login", async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      "secretkey",
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token
+    });
 
   } catch (error) {
     res.status(500).json({ error: error.message });
