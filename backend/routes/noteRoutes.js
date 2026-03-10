d1bf9dd2bd851c479a95941e9420b0581994cdeb
Risidu03 <geeganagerisidu@gmail.com>
2026-03-10 10:48:11 +0530

    Add update and delete notes APIs

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -48,4 +48,59 @@ router.get("/", authMiddleware, async (req, res) => {
 });
 
 
+// UPDATE NOTE
+router.put("/:id", authMiddleware, async (req, res) => {
+
+  try {
+
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    if (note.owner.toString() !== req.user.id) {
+      return res.status(403).json({ message: "Not allowed" });
+    }
+
+    note.title = req.body.title || note.title;
+    note.content = req.body.content || note.content;
+
+    await note.save();
+
+    res.json(note);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+});
+
+
+// DELETE NOTE
+router.delete("/:id", authMiddleware, async (req, res) => {
+
+  try {
+
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    if (note.owner.toString() !== req.user.id) {
+      return res.status(403).json({ message: "Not allowed" });
+    }
+
+    await note.deleteOne();
+
+    res.json({ message: "Note deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+});
+
+
 module.exports = router;
